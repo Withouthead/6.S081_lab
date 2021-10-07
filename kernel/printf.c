56485215fcf661aa8f17e79d608b59993e738e9d
xiaomaotou31 <xiaomaotou31@gmail.com>
2021-10-07 11:54:18 +0800

    完成lab4 -2

--- kernel/printf.c
+++ kernel/printf.c
@@ -132,3 +132,19 @@ printfinit(void)
   initlock(&pr.lock, "pr");
   pr.locking = 1;
 }
+
+void backtrace()
+{
+  uint64 fp =  r_fp();
+  uint64 hight, low;
+  hight = PGROUNDUP(fp);
+  low = PGROUNDDOWN(fp);
+  printf("backtrace:\n");
+  while(fp >= low && fp < hight)
+  {
+    uint64 *fp_pointer = (uint64 *)fp;
+    printf("%p\n", *(uint64 *)(fp - 8));
+    fp = (*(uint64 *)(fp - 16));
+  }
+
+}

399195c4da3e99fa5fbb9f971a9ac389f8d62e4c
xiaomaotou31 <xiaomaotou31@gmail.com>
2021-10-18 15:23:42 +0800

    修改完成uvmcopy

--- kernel/vm.c
+++ kernel/vm.c
@@ -320,11 +320,8 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
       panic("uvmcopy: page not present");
     pa = PTE2PA(*pte);
     flags = PTE_FLAGS(*pte);
-    if((mem = kalloc()) == 0)
-      goto err;
-    memmove(mem, (char*)pa, PGSIZE);
-    if(mappages(new, i, PGSIZE, (uint64)mem, flags) != 0){
-      kfree(mem);
+
+    if(mappages(new, i, PGSIZE, pa, flags) != 0){
       goto err;
     }
     *pte = (*pte) & (~PTE_W);
